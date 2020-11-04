3e58e3b09850d8768e328af7d295fea29a957d8f
AHIR CHIRAG <72852725+Chirag-ahir@users.noreply.github.com>
2020-11-04 11:15:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+------pip install pywhatkit--------
+import pywhatkit
+pywhatkit.sendwhatmsg("+91********","good morning",time hour,time minute)
+
+#you have to login in your whatsapp web before run this program.
